568602700fa1426d0a0ec2762a7f67c660b9e24d
Cheeze-Man <pksam5046@naver.com>
2023-09-07 18:04:35 +0900

    Add : 공통 컴포넌트 레이아웃 작업 완료

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import CalendarModal from '../../components/CalendarModal/CalendarModal';
+import DefaultButton from '../../components/DefaultButton/DefaultButton';
+import DefaultInput from '../../components/DefaultInput/DefaultInput';
+import { CiUser, CiLock } from 'react-icons/ci';
+import './Main.scss';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      {/* <CalendarModal /> */}
+      <div className="main">
+        Main
+        <DefaultInput
+          icon={<CiUser className="inputIcon" />}
+          type="text"
+          placeholder="전화번호"
+        />
+        <DefaultInput
+          icon={<CiLock className="inputIcon" />}
+          type="password"
+          placeholder="비밀번호"
+        />
+        <DefaultButton />
+      </div>
+    </>
+  );
 };
 
 export default Main;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,58 @@
+import {
+  LiaHomeSolid,
+  LiaCoinsSolid,
+  LiaUserFriendsSolid,
+  LiaEllipsisHSolid,
+} from 'react-icons/lia';
+
+/** Email pattern for validation */
 export const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+/** Buttons objects in "Array" for Navigation bar */
+export const NAVIGATION_BUTTONS = [
+  {
+    name: '가계부',
+    path: '/main',
+    icon: <LiaHomeSolid className="navIcons" />,
+  },
+  {
+    name: '자산',
+    path: '/main',
+    icon: <LiaCoinsSolid className="navIcons" />,
+  },
+  {
+    name: '공동관리',
+    path: '/main',
+    icon: <LiaUserFriendsSolid className="navIcons" />,
+  },
+  {
+    name: '더보기',
+    path: '/main',
+    icon: <LiaEllipsisHSolid className="navIcons" />,
+  },
+];
+
+/** Calendar data for Calendar Modal */
+export const CALENDAR_MODAL_LIST = [
+  { title: '2023년 9월' },
+  { title: '2023년 8월' },
+  { title: '2023년 7월' },
+  { title: '2023년 6월' },
+  { title: '2023년 5월' },
+  { title: '2023년 4월' },
+  { title: '2023년 3월' },
+  { title: '2023년 2월' },
+  { title: '2023년 1월' },
+  { title: '2022년 12월' },
+  { title: '2022년 11월' },
+  { title: '2022년 10월' },
+  { title: '2022년 9월' },
+  { title: '2022년 8월' },
+  { title: '2022년 7월' },
+  { title: '2022년 6월' },
+  { title: '2022년 5월' },
+  { title: '2022년 4월' },
+  { title: '2022년 3월' },
+  { title: '2022년 2월' },
+  { title: '2022년 1월' },
+];
